4589327a7695cd210c2ac411963f8e13d69c429e
NuminousNonentity <60616351+NuminousNonentity@users.noreply.github.com>
2022-09-30 12:16:42 +0100

    Create #4.py

---
+++ #4.py
@@ -0,0 +1,5 @@
+sal = int(input("What is your yearly salery? :"))
+msal = str(sal/12)
+print("Monthly salery before tax is £"+msal)
+mtsal = str((sal/12)*0.8)
+print("Monthly salery after tax is £"+mtsal)
