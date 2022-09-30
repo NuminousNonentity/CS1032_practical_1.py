6cf1738e864a3cb2a9cd77cdcb708843192714fc
NuminousNonentity <60616351+NuminousNonentity@users.noreply.github.com>
2022-09-30 12:19:06 +0100

    Create #7.py

---
+++ #7.py
@@ -0,0 +1,6 @@
+c = 3*10**8
+time = int(input("Enter an amount of time in seconds: "))
+M = c * time
+KM = M / 10**3
+m = M * 0.000621371
+print("In that time, light could have travelled",M,"meteres or",KM,"kilometers or",m,"miles")
